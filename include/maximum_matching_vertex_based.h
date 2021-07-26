4778571b092d28bba277299cc19e8e23895fd893
Ahmed Abbas <ahmed.a.mirza1@gmail.com>
2021-07-26 12:34:20 +0200

    Changed matching threshold to 0.

--- include/maximum_matching_vertex_based.h
+++ include/maximum_matching_vertex_based.h
@@ -2,4 +2,4 @@
 
 #include "dCOO.h"
 
-std::tuple<thrust::device_vector<int>, int> filter_edges_by_matching_vertex_based(const dCOO& A, const float retain_ratio = 0.5);
+std::tuple<thrust::device_vector<int>, int> filter_edges_by_matching_vertex_based(const dCOO& A, const float retain_ratio = 0.0);
