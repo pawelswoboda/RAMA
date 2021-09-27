6b7585d00f3bb22d34b76bcc242d6fd34ffbbec5
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2021-09-27 11:06:53 +0200

    fixed early termination for mode "D"

--- src/dual_solver.cu
+++ src/dual_solver.cu
@@ -8,7 +8,8 @@ std::tuple<dCOO, double, int> dual_update_cycle_length(const dCOO& A, const int 
 {
     thrust::device_vector<int> triangles_v1, triangles_v2, triangles_v3;
     std::tie(triangles_v1, triangles_v2, triangles_v3) = conflicted_cycles_cuda(A, cycle_length, tri_memory_factor, tol_ratio);
-    if (triangles_v1.size() == 0)
+    const int num_tri = triangles_v1.size();
+    if (num_tri == 0)
         return {A, get_lb(A.get_data()), 0};
 
     multicut_message_passing mp(A, std::move(triangles_v1), std::move(triangles_v2), std::move(triangles_v3));
@@ -28,7 +29,7 @@ std::tuple<dCOO, double, int> dual_update_cycle_length(const dCOO& A, const int 
     thrust::device_vector<float> costs_repam;
     std::tie(i_repam, j_repam, costs_repam) = mp.reparametrized_edge_costs();
     dCOO A_repam(std::move(j_repam), std::move(i_repam), std::move(costs_repam), true, true);
-    return {A_repam, final_lb, triangles_v1.size()};
+    return {A_repam, final_lb, num_tri};
 }
 
 double dual_solver(dCOO& A, const int max_cycle_length, const int num_iter, const float tri_memory_factor, const int num_outer_itr, const float tol_ratio)
