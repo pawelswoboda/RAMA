b64e8bf4c2f29eafeb3d098460129c2d8a4a9527
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2021-10-05 15:20:31 +0200

    bump py version

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='RAMA',
-    version='0.0.3',
+    version='0.0.4',
     description='Bindings for RAMA: Rapid algorithm for multicut.',
     packages=find_packages('.'),
     ext_modules=[CMakeExtension(name='rama_py')],
